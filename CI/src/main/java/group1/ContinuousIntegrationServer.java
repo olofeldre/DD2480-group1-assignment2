839396c74af1042038dadf243c4af28482f18935
Joakim Skoog <joaskoog@kth.se>
2021-02-09 17:01:06 +0100

    Feature #7 Make a server respond to a github webhook.
    This commit shows how to make a server that can recognize
    a github webhook and see wich commit has been posted. The
    webhook needs to be in json and the URL needs to be the same
    as the forwarded ngrok URL. The webhook should only trigger
    on push events.
    
    Co-authored-by Olof Gren <olofthegren@gmail.com>

--- CI/src/main/java/group1/ContinuousIntegrationServer.java
+++ CI/src/main/java/group1/ContinuousIntegrationServer.java
@@ -8,7 +8,12 @@ import java.io.IOException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import java.util.Enumeration;
+import java.io.BufferedReader;
+import java.util.stream.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
  See the Jetty documentation for API documentation of those classes.
@@ -26,11 +31,32 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
+	if (baseRequest.getMethod().equals("POST")){
+		BufferedReader cont = request.getReader();
+		Stream<String> linesStream = cont.lines();
+
+		//String[] lines = linesStream.toArray();
+		Object[] lines = linesStream.toArray();
+		
+	       	String lineJson = (String) lines[0];
+
+		final String pattern ="\\\"after\\\":\\\"([0-9a-f]{0,41})\\\",";
+		// Create a Pattern object
+		Pattern r = Pattern.compile(pattern);
+
+		// Now create matcher object.
+		Matcher m = r.matcher(lineJson);
+		while(m.find()) {
+		    System.out.println("Recognize the ID of commit : " + m.group(1));
+		}
+		//System.out.println("Found value: " + m.group(0) );
+	}
 
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
+	// 3nd run tests
 
         response.getWriter().println("CI job done");
     }
@@ -38,7 +64,8 @@ public class ContinuousIntegrationServer extends AbstractHandler
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8080);
+	final int portNr = 8001; 
+        Server server = new Server(portNr);
         server.setHandler(new ContinuousIntegrationServer()); 
         server.start();
         server.join();
