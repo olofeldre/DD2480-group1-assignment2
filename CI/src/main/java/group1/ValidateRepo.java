6c37cae41c3c045632e401a7e946c164024573d9
clarse <clarse@kth.se>
2021-02-15 15:52:20 +0100

    doc #10 Add a class to the CI server that clones the repo
    This adds javadoc to the ValidateRepo file.
    
    Co-authored-by: Caroline Larsen <e.caroline.larsen@gmail.com>
    Co-authored-by: Anton Lövström <anton.lovstrom4@gmail.com>

--- CI/src/main/java/group1/ValidateRepo.java
+++ CI/src/main/java/group1/ValidateRepo.java
@@ -10,7 +10,13 @@ import java.util.ArrayList;
 class ValidateRepo {
 
     public ValidateRepo() {}
-
+    /**
+     * Clones the specified branch of the specified repo into temp/repo
+     * 
+     * @param repoURI   an URL to the repo
+     * @param branch    the branch name
+     * @return          true if the clone was successful, false otherwise
+     */ 
     //repoURI should be "https://github.com/olofeldre/DD2480-group1-assignment2"
     public static boolean CloneRepo(String repoURI, String branch) {
         String branchPath = "refs/heads/" + branch;
@@ -29,7 +35,13 @@ class ValidateRepo {
             return false;
         }
     }
-
+    /**
+     * Validates the specified repo by cloning, compiling and running tests
+     * 
+     * @param repoURI   an URL to the repo
+     * @param branch    the branch name
+     * @return          true if the validation was successful, false otherwise
+     */ 
     public static boolean Validate(String repoURI, String branch) {
         boolean check = CloneRepo(repoURI, branch);
         // TODO: call other functions to compile, test and cleanup
