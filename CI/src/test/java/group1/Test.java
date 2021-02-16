5105d2ddfc5b9082c7d166d3d91d528877dac410
Anton Lövström <antonlov@kth.se>
2021-02-16 11:53:07 +0100

    feature #25 Create branches for testing purposes
    Add a branch with a test that always passes.

---
+++ CI/src/test/java/group1/Test.java
@@ -0,0 +1,13 @@
+package group1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Test {
+    // Test that passes
+    @Test
+    public void cloneRepo_Valid()
+    {
+        assertTrue(true);
+    }
+}
