8b03ac1cd1e48ac2c35d33e3e5d5b302f37606a7
Anton Lövström <antonlov@kth.se>
2021-02-16 16:44:49 +0100

    feature #30 Send email upon recieving data from the webhook
    This implements continous integration by sending an email when something pushed, with the build results.
    
    Co-authored-by: Olof Gren <olofthegren@gmail.com>

--- CI/src/main/java/group1/ContinuousIntegrationServer.java
+++ CI/src/main/java/group1/ContinuousIntegrationServer.java
@@ -20,6 +20,14 @@ import java.util.regex.Pattern;
 */
 public class ContinuousIntegrationServer extends AbstractHandler
 {
+
+    private final String REPO_URI = "https://github.com/olofeldre/DD2480-group1-assignment2";
+    private final String[] EMAIL_ADDRESSES = new String[]{"anton.lovstrom4@gmail.com", 
+                                                          "joakim.skoog.97@gmail.com", 
+                                                          "e.caroline.larsen@gmail.com", 
+                                                          "olofthegren@gmail.com",
+                                                          "robin.wanlund@hotmail.com"};
+
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
@@ -31,30 +39,29 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
-	if (baseRequest.getMethod().equals("POST")){
-		BufferedReader cont = request.getReader();
-		Stream<String> linesStream = cont.lines();
+        if (baseRequest.getMethod().equals("POST")){
+            BufferedReader cont = request.getReader();
+            Stream<String> linesStream = cont.lines();
 
-		Object[] lines = linesStream.toArray();
+            Object[] lines = linesStream.toArray();
 
-	       	String lineJson = (String) lines[0];
+            String lineJson = (String) lines[0];
 
-		final String pattern ="\\\"after\\\":\\\"([0-9a-f]{0,41})\\\",";
-		// Create a Pattern object
-		Pattern r = Pattern.compile(pattern);
+            JSONExtractor extractor = new JSONExtractor(lineJson);
+            
+            String branch = extractor.getBranch();
+            String commitID = extractor.getCommit();
 
-		// Now create matcher object.
-		Matcher m = r.matcher(lineJson);
-		while(m.find()) {
-		    System.out.println("Recognize the ID of commit : " + m.group(1));
-		}
-	}
+            boolean passedTests = ValidateRepo.Validate(REPO_URI, branch);
 
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-	// 3nd run tests
+            final String messageContents = "Build results from branch " + branch + " of " + REPO_URI + "\n\n"
+                                    + "Commit ID: " + commitID + "\n"
+                                    + "Branch: " + branch + "\n"
+                                    + "Passed tests: " + (passedTests ? "YES" : "NO") + "\n";
+            for (String email : EMAIL_ADDRESSES) {
+                Email.sendEmailAndReportSuccess(email, "Build results from " + REPO_URI, messageContents);
+            }
+        }
 
         response.getWriter().println("CI job done");
     }
